a73e2e7b5b1eb40d7383dc6747aba4f4f4152a46
jsbednarski <jsbednarski@github.com>
2023-07-06 23:10:29 +0200

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Hero from '@/components/hero';
+
+export default function Home() {
+  return (
+      <main className="overflow-hidden">
+        <Hero />
+      </main>
+  );
+}
